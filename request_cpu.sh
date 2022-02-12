73e0f9c9136b07044915c7eab7456f209fff4d7f
yaqi yu <74639908+nothairyyq@users.noreply.github.com>
2022-02-12 19:29:55 +0800

    Add files via upload

---
+++ request_cpu.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#SBATCH -N 1
+#SBATCH --partition=batch
+#SBATCH -J MyJob
+#SBATCH -o MyJob.%J.out
+#SBATCH -e MyJob.%J.err
+#SBATCH --mail-user=yaqi.yu2000@gmail.com
+#SBATCH --mail-type=ALL
+#SBATCH --time=03:30:00
+#SBATCH --mem=50G
+#SBATCH --constraint=[cascadelake]
+
+#run the application:
+Myapp
